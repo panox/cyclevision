246408772ec211afae4456ec359e3e77b11dcd1f
panox <panospalos@gmail.com>
2015-11-21 13:19:49 +0000

    usersIndex method

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,5 +1,12 @@
 var User   = require('../models/user');
 
+function usersIndex(req, res) {
+  User.find(function(err, users){
+    if (err) return res.status(404).json({message: 'No users found'});
+    res.status(200).json({ users: users });
+  });
+}
+
 module.exports = {
   usersIndex:  usersIndex
 }
