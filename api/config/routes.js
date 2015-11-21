d3b393f97f66b49b87c0589622be8240be7552a0
panox <panospalos@gmail.com>
2015-11-21 13:22:13 +0000

    comment out routes that do not exist yet

--- api/config/routes.js
+++ api/config/routes.js
@@ -3,26 +3,27 @@ var router   = express.Router();
 var passport = require("passport");
 
 var usersController = require('../controllers/usersController');
-var imagesController = require('../controllers/imagesController');
+// var imagesController = require('../controllers/imagesController');
+// controller does not exist yet
 
 router.route('/users')
-  .get(usersController.usersIndex)
-  .post(usersController.userCreate)
+  .get(usersController.usersIndex);
+  // .post(usersController.userCreate)
 
 
-router.route('/users/:id')
-  .get(usersController.userShow)
-  .put(usersController.userUpdate)
-  .delete(usersController.userDelete);
+// router.route('/users/:id')
+//   .get(usersController.userShow)
+//   .put(usersController.userUpdate)
+//   .delete(usersController.userDelete);
 
-router.route('/images')
-  .get(imagesController.imagesIndex)
-  .post(imagesController.imageCreate)
+// router.route('/images')
+//   .get(imagesController.imagesIndex)
+//   .post(imagesController.imageCreate)
 
 
-router.route('/images/:id')
-  .get(imagesController.imageShow)
-  .put(imagesController.imageUpdate)
-  .delete(imagesController.imageDelete);
+// router.route('/images/:id')
+//   .get(imagesController.imageShow)
+//   .put(imagesController.imageUpdate)
+//   .delete(imagesController.imageDelete);
 
 module.exports = router
