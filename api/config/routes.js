fe9d7c3790a5e3e72cb5fc5bbe98781b097dda11
panox <panospalos@gmail.com>
2015-11-21 13:10:02 +0000

    require images controller

--- api/config/routes.js
+++ api/config/routes.js
@@ -3,6 +3,7 @@ var router   = express.Router();
 var passport = require("passport");
 
 var usersController = require('../controllers/usersController');
+var imagesController = require('../controllers/imagesController');
 
 router.route('/users')
   .get(usersController.usersIndex)
