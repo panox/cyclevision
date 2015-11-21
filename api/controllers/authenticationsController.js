0c91712693251d5fcf06f4ac16a96bf1e1cf22b3
panox <panospalos@gmail.com>
2015-11-21 15:55:24 +0000

    signup takes too long to signup

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -1,13 +1,13 @@
 var passport = require("passport");
 
-function signup(req, res, next) {
-  var localStrategy = passport.authenticate('local-signup', function(err, user){
+function signup(req, res) {
+  var signupStrategy = passport.authenticate('local-signup', function(err, user){
     if (err) return res.status(500).json({ message: 'Signup did not finish' });
     if (!user) return res.status(401).json({ message: 'User already exists!' });
+  });
+  return signupStrategy(req, res);
+}
 
-  return localStrategy(req, res, next);
-  })
-};
 
 module.exports = {
   signup: signup
