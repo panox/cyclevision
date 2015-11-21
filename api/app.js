3909b0920aa98d2bfbcbb1e2aafde24140415b6f
panox <panospalos@gmail.com>
2015-11-21 12:31:00 +0000

    app require modules and app listen

---
+++ api/app.js
@@ -0,0 +1,26 @@
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var mongoose       = require('mongoose');
+var passport       = require('passport');
+var cookieParser   = require("cookie-parser");
+var methodOverride = require("method-override");
+var jwt            = require('jsonwebtoken');
+var expressJWT     = require('express-jwt');
+var app            = express();
+
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.listen(3000);
+console.log('listening to 3000')
