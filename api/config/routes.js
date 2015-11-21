29c6c9717c772b8cb214aa83c4aa818b87d2e268
panox <panospalos@gmail.com>
2015-11-21 13:00:54 +0000

    routes for users

---
+++ api/config/routes.js
@@ -0,0 +1,17 @@
+var express  = require('express');
+var router   = express.Router();
+var passport = require("passport");
+
+var usersController = require('../controllers/usersController');
+
+router.route('/users')
+  .get(usersController.usersIndex)
+  .post(usersController.userCreate)
+
+
+router.route('/users/:id')
+  .get(usersController.userShow)
+  .put(usersController.userUpdate)
+  .delete(usersController.userDelete);
+
+module.exports = router
