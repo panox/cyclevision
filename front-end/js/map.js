95dbaf77d62cb95a15807d4e50bd921d819057bc
panox <panospalos@gmail.com>
2015-11-22 18:59:05 +0000

    request for google

--- front-end/js/map.js
+++ front-end/js/map.js
@@ -1,5 +1,13 @@
+$(function() {
+
+  var amphitheatreParkway = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+
+});
+
+var myLatLng = {lat: -25.363, lng: 131.044};
+
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
+  
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
