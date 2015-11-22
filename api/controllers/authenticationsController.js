680295608ce839a3191f522b02af8041e39d0f6b
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-22 15:07:48 +0000

    some changes still unexpected token errot

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -27,7 +27,7 @@ function login(req, res, next) {
   var loginStrategy = passport.authenticate('local-login', function(err, user) {
     if(err || !user) res.status(401).json({ message: 'An error occured' });
     var token = jwt.sign(user, secret({ expiresIn: 60*60*24 }));
-    return res.status(200).json({ token: token, user: user });
+    return res.status(200).json({ token: token, user: user })
   });
   return loginStrategy(req, res);
 };
