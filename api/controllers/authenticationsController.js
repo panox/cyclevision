7ed104524811dfec8423344b2b58fc1f9133b2c5
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-22 13:54:11 +0000

    added login and jwt authentication to authentications controller

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -1,14 +1,29 @@
 var passport = require("passport");
+var User     = require("../models/user")
+var secret   = require("../config/config").secret
+var jwt      = require('jsonwebtoken')
 
 function signup(req, res) {
   var signupStrategy = passport.authenticate('local-signup', function(err, user){
     if (err) return res.status(500).json({ message: 'Signup did not finish' });
     if (!user) return res.status(401).json({ message: 'User already exists!' });
+    var token = jwt.sign(user, secret { expiresIn: 60*60*24 });
+    return res.status(200).json({ token: token, user: user})
   });
   return signupStrategy(req, res);
 }
 
+function login(req, res, next) {
+  var loginStrategy = passport.authenticate('local-login', function(err, user) {
+    if(err || !user) res.status(401).json({ message: 'An error occured' });
+    var token = jwt.sign(user, secret { expiresIn 60*60*24});
+    return res.status(200).json({ token: token, user: user });
+  });
+  return loginStrategy(req, res);
+};
+
 
 module.exports = {
-  signup: signup
+  signup: signup,
+  login: login
 }
