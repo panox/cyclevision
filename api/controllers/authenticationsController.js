d5683e5b8c3d2ff80f3e2d1ed5ba95301c4f24d9
panox <panospalos@gmail.com>
2015-11-22 14:11:31 +0000

    error returns message

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -3,8 +3,8 @@ var User     = require('../models/user');
 
 function signup(req, res, next) {
   var localStrategy = passport.authenticate('local-signup', function(err, user, message) {
-    if (err) return res.status(500).json({ message: 'Something went wrong!' });
-    if (!user) return res.status(401).json({ user: user, message: message });
+    if (err) return res.status(500).json({ message });
+    if (!user) return res.status(401).json({ message });
 
     return res.status(200).json({ 
       success: true,
