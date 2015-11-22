1ad17373f4456b0d34ffbaa7efcfb0102b93fef5
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-22 14:38:45 +0000

    JWT authentication added

--- api/config/config.js
+++ api/config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'database': 'mongodb://localhost:27017/cyclevision'
+  'database': 'mongodb://localhost:27017/cyclevision',
+  'secret'  : 'process.env.SECRET'
 };
--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -1,13 +1,13 @@
 var passport = require("passport");
 var User     = require("../models/user")
-var secret   = require("../config/config").secret
 var jwt      = require('jsonwebtoken')
+var secret   = require('../config/config').secret
 
 function signup(req, res) {
   var signupStrategy = passport.authenticate('local-signup', function(err, user){
     if (err) return res.status(500).json({ message: 'Signup did not finish' });
     if (!user) return res.status(401).json({ message: 'User already exists!' });
-    var token = jwt.sign(user, secret { expiresIn: 60*60*24 });
+    var token = jwt.sign(user, secret ({expiresIn: 60*60*24 }));
     return res.status(200).json({ token: token, user: user})
   });
   return signupStrategy(req, res);
@@ -16,7 +16,7 @@ function signup(req, res) {
 function login(req, res, next) {
   var loginStrategy = passport.authenticate('local-login', function(err, user) {
     if(err || !user) res.status(401).json({ message: 'An error occured' });
-    var token = jwt.sign(user, secret { expiresIn 60*60*24});
+    var token = jwt.sign(user, secret({ expiresIn: 60*60*24 }));
     return res.status(200).json({ token: token, user: user });
   });
   return loginStrategy(req, res);
