093e48175466a5fd9aa4b4f4c7abcf44287a5858
panox <panospalos@gmail.com>
2015-11-22 18:33:38 +0000

    google map key open

--- front-end/js/map.js
+++ front-end/js/map.js
@@ -1,7 +1,14 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
   });
 }
