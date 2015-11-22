a034e1b48b0823975962ef8c3bd0208758a871a3
panox <panospalos@gmail.com>
2015-11-22 13:30:10 +0000

    if signup sucess return 200

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -1,11 +1,10 @@
 var passport = require("passport");
 
 function signup(req, res) {
-  var signupStrategy = passport.authenticate('local-signup', function(err, user){
-    if (err) return res.status(500).json({ message: 'Signup did not finish' });
-    if (!user) return res.status(401).json({ message: 'User already exists!' });
-  });
-  return signupStrategy(req, res);
+  return passport.authenticate('local-signup', function(err, user){
+    if (err) return res.status(500).json({ message: err });
+    res.status(200).json({ user:user})
+  })(req, res);
 }
 
 
