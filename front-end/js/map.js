b2e48cf1a56165c79215b8f96746e6ed0d4bcbea
panox <panospalos@gmail.com>
2015-11-22 19:29:32 +0000

    get location from image and create pin

--- front-end/js/map.js
+++ front-end/js/map.js
@@ -1,31 +1,37 @@
 $(function() {
 
-  var amphitheatreParkway = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
-  var secondHome = "https://maps.googleapis.com/maps/api/geocode/json?address=68 Hanbury St London E1 5JL"
-
-  $.get(secondHome)
+  $.get("http://localhost:3000/api/images/5652162845ace0f652a568a3")
   .done(function(res){
-    var location = res.results[0].geometry.location
+    var imageLocation = res.image.location
+    var secondHome = "https://maps.googleapis.com/maps/api/geocode/json?address=" + imageLocation
 
-    var myLatLng = {lat: location.lat, lng: location.lng};
+    $.get(secondHome)
+    .done(function(res){
+      var location = res.results[0].geometry.location
+
+      var myLatLng = {lat: location.lat, lng: location.lng};
 
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 18,
-      center: myLatLng
-    });
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: myLatLng
+      });
 
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Hello World!'
-    });
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+      });
 
+    })
+    .fail(function(res){
+      console.log("Error making map")
+    })
 
 
   })
   .fail(function(res){
-    console.log("An error")
+    console.log("Error getting values")
   })
 
 });
