f1472345e009adcfcf36bb4f0e8b4662ca03ba57
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-23 12:07:49 +0000

    token in local storage

--- front-end/js/login_form.js
+++ front-end/js/login_form.js
@@ -1,3 +1,43 @@
 $(function(){ 
 
+  function init(){
+  $("form").on("submit", submitForm);
+
+  }
+
+  function submitForm(req, res){
+    event.preventDefault();
+
+    var method = $(this).attr("method");
+    var url    = "http://localhost:3000/api/login";
+    var data   = $(this).serialize();
+
+    return ajaxRequest(method, url, data)
+  }
+
+  function setToken(){
+    return window.localStorage.setItem("token", token);
+  }
+
+  function authenticationSuccessful(){
+
+  }
+
+  function ajaxRequest(method, url, data){
+    return $.ajax({
+    method: method,
+    url: url,
+    data: data,
+    }).done(function(data){
+      console.log(data);
+    }).fail(function(data) {
+      console.log(data.responseJSON.message);
+    });
+  }
+
+  init()
+
+
+
+
 })
