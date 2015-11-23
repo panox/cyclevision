013f666a7e54edaf0382958129f341fbfa19e5d2
panox <panospalos@gmail.com>
2015-11-23 14:15:41 +0000

    users and images ajax requests

--- front-end/js/ajax.js
+++ front-end/js/ajax.js
@@ -18,6 +18,11 @@ $(function() {
   $.get("http://localhost:3000/api/users")
   .done(function(res){
     console.log(res.users)
+    _(res.users).each(function(item) {
+      var underscoreTemplate = _.template($('#users-template').html());
+      var compiledTemplate = underscoreTemplate(item);
+      $('#users').append(compiledTemplate);
+    })
   })
   .fail(function(res){
     console.log("Error getting values")
