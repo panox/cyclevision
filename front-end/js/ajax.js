51266d71e54a79e493c1d21d9488045059af1b9f
panox <panospalos@gmail.com>
2015-11-23 13:44:30 +0000

    on load function

--- front-end/js/ajax.js
+++ front-end/js/ajax.js
@@ -1,13 +1,20 @@
-function ajaxRequest(method, url, data){
-  return $.ajax({
-  method: method,
-  url: url,
-  data: data,
-  beforeSend: setRequestHeader,
-  }).done(function(data){
-    console.log(data);
-    authenticationSuccessful(data);
-  }).fail(function(data) {
-    console.log(data.responseJSON.message);
-  });
-}
+$(function() {
+
+  function ajaxRequest(method, url, data){
+    return $.ajax({
+    method: method,
+    url: url,
+    data: data,
+    beforeSend: setRequestHeader,
+    }).done(function(data){
+      console.log(data);
+      authenticationSuccessful(data);
+    }).fail(function(data) {
+      console.log(data.responseJSON.message);
+    });
+  }
+
+  
+
+
+});
