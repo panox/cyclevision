a10efb8075c1edff13cbb6d93c8dff8061b1fbf0
panox <panospalos@gmail.com>
2015-11-23 15:20:35 +0000

    ajax request for user

--- front-end/js/user.js
+++ front-end/js/user.js
@@ -1,9 +1,16 @@
 $(function() {
 
-  function getToken() {
-    return localStorage.getItem("token");
-  }
+  
+  var userId = localStorage.getItem("token");
+  var url = "http://localhost:3000/api/users/" + userId
+  console.log(userId)
 
-  console.log(getToken());
+  $.get(url)
+  .done(function(res){
+    console(res)
+  })
+  .fail(function(res){
+    console.log("Error making map")
+  })
 
 });
