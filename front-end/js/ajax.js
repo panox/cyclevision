df1c77041ec16202fdb389817ea203b98fafb1fc
panox <panospalos@gmail.com>
2015-11-23 13:48:11 +0000

    get users

--- front-end/js/ajax.js
+++ front-end/js/ajax.js
@@ -3,7 +3,15 @@ $(function() {
   // get images
   $.get("http://localhost:3000/api/images")
   .done(function(res){
-    console.log(res)
+    console.log(res.images)
+  })
+  .fail(function(res){
+    console.log("Error getting values")
+  })
+  // get users
+  $.get("http://localhost:3000/api/users")
+  .done(function(res){
+    console.log(res.users)
   })
   .fail(function(res){
     console.log("Error getting values")
