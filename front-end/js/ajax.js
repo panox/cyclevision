c915359139a0681bbe35965436d420f7f327142e
panox <panospalos@gmail.com>
2015-11-23 13:42:44 +0000

    ajax request method

---
+++ front-end/js/ajax.js
@@ -0,0 +1,13 @@
+function ajaxRequest(method, url, data){
+  return $.ajax({
+  method: method,
+  url: url,
+  data: data,
+  beforeSend: setRequestHeader,
+  }).done(function(data){
+    console.log(data);
+    authenticationSuccessful(data);
+  }).fail(function(data) {
+    console.log(data.responseJSON.message);
+  });
+}
