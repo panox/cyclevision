7e7471f95487a773ed898e8c4e83455577066bcc
panox <panospalos@gmail.com>
2015-11-23 17:57:42 +0000

    create a data variable to be passed to underscore template

--- front-end/js/user.js
+++ front-end/js/user.js
@@ -6,14 +6,16 @@ $(function() {
 
   $.get(url)
   .done(function(res){
-    console.log(res.user._id)
-    var item = _(res.user._id)
-    console.log("Under " + item)
-    var underscoreTemplate = _.template($('#user-template').html());
+    var user = res.user;
+    var data = {
+      email: user.local.email
+    }
 
-    // var compiledTemplate = underscoreTemplate(item);
-    // console.log(compiledTemplate)
-    // $('#current-user-name').append(compiledTemplate);
+    var template = _.template($( '#user-template' ).html());
+    var underscoreTemplate = _.template($('#user-template').html());
+    var compiledTemplate = underscoreTemplate(data);
+    $('#current-user-name').append(compiledTemplate);
+
   })
   .fail(function(res){
     console.log("Error finding user")
