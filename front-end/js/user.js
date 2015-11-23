3c20f9260069ec6b9fdee6028627edcb557d6d2a
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-23 16:08:00 +0000

    underscore linking correctly

--- front-end/js/user.js
+++ front-end/js/user.js
@@ -7,9 +7,19 @@ $(function() {
   $.get(url)
   .done(function(res){
     console.log(res)
+    _(res.user.local.email)
   })
   .fail(function(res){
-    console.log("Error making map")
+    console.log("Error finding user")
   })
+
+  //   console.log(res.images)
+  //   _(res.images).each(function(item) {
+  //     var underscoreTemplate = _.template($('#images-template').html());
+  //     var compiledTemplate = underscoreTemplate(item);
+  //     $('#images').append(compiledTemplate);
+  //   })
+
+  // })
 
 });
