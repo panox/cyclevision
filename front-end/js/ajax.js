3813bf2b5ea1a2b10018dee98cbb6680e890fb0e
panox <panospalos@gmail.com>
2015-11-23 14:04:55 +0000

    get images and append with underscore

--- front-end/js/ajax.js
+++ front-end/js/ajax.js
@@ -4,6 +4,12 @@ $(function() {
   $.get("http://localhost:3000/api/images")
   .done(function(res){
     console.log(res.images)
+    _(res.images).each(function(item) {
+      var underscoreTemplate = _.template($('#images-template').html());
+      var compiledTemplate = underscoreTemplate(item);
+      $('#images').append(compiledTemplate);
+    })
+
   })
   .fail(function(res){
     console.log("Error getting values")
