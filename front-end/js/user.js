867e6be98da6baab5b25ee4ff7b8a966c0ec8d5c
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-24 10:14:21 +0000

    delete for user image working

--- front-end/js/user.js
+++ front-end/js/user.js
@@ -1,11 +1,30 @@
+function setHeaders(xhr) {
+  var token = localStorage.getItem('token');
+  if(token) xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+}
+
+function ajaxRequest(url, method, data, callback) {
+  $.ajax({
+    url: url,
+    method: method,
+    data: data,
+    beforeSend: setHeaders
+  }).done(function(res) {
+    return callback(res);
+  }).fail(function(err) {
+    console.error(err);
+  });
+}
+
+var apiURL = "http://localhost:3000/api/";
+
 $(function() {
 
   
   var userId = localStorage.getItem("userId");
-  var url = "http://localhost:3000/api/users/" + userId;
+  var url = apiURL + "users/" + userId;
 
-  $.get(url)
-  .done(function(res){
+  ajaxRequest(url, "GET", null, function(res){
     var user = res.user;
     var data = {
       email: user.local.email,
@@ -27,15 +46,17 @@ $(function() {
       var underscoreTemplate = _.template($('#user-images').html());
       var compiledTemplate = underscoreTemplate(item);
       $('#images').append(compiledTemplate);
-      $('#images').on('click', $("#delete-image"), function(){
-        $.delete(url)
-        .done(function())
-      } )
+      $('#images').on('click', "#delete-image", function(){
+        event.preventDefault();
+        var $image = $(event.target).parent('.image');
+        console.log("click");
+        console.log($image);
+        ajaxRequest(apiURL + "images/" + item._id, "DELETE", null, function() {
+          $image.remove();
+        });
+      })
     })
 
-  })
-  .fail(function(res){
-    console.log("Error finding user")
-  })
+  });
 
 });
