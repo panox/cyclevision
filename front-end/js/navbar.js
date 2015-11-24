c65fb5e33c9bea3b131007000b1ad75653a0b322
panox <panospalos@gmail.com>
2015-11-24 13:38:48 +0000

    removed console log

--- front-end/js/navbar.js
+++ front-end/js/navbar.js
@@ -2,13 +2,12 @@ function addLogout() {
 
   var underscoreTemplate = _.template($('#logout').html());
   var compiledTemplate = underscoreTemplate();
-  console.log(compiledTemplate)
   $('.hide-on-med-and-down').append(compiledTemplate);
   $('#mobile-demo').append(compiledTemplate);
 }
 
 $(function(){ 
-  
+
   if(getToken()) {
     addLogout()
   }
