2a07ed94359999b1a556fc688c703600e5836093
panox <panospalos@gmail.com>
2015-11-24 10:12:01 +0000

    users source null values to be extended

--- front-end/js/users.js
+++ front-end/js/users.js
@@ -4,7 +4,14 @@ $.get("http://localhost:3000/api/users")
   .done(function(res){
   console.log(res.users)
   _(res.users).each(function(item) {
-    source = {first_name: null}
+    source = {
+      first_name: null,
+      last_name: null,
+      profile_pic: null, 
+      type_of_cyclist: null,
+      about_me: null, 
+      city: null
+    }
     data = _.extend(source, item)
     var underscoreTemplate = _.template($('#all-users').html());
     console.log(item)
