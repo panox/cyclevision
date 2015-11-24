d4cdbc4508fb6c1e20c9cfc06590812d69c44765
panox <panospalos@gmail.com>
2015-11-24 10:27:05 +0000

    token js in users html

---
+++ front-end/js/token.js
@@ -0,0 +1,17 @@
+function setHeaders(xhr) {
+  var token = localStorage.getItem('token');
+  if(token) xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+}
+
+function ajaxRequest(url, method, data, callback) {
+  $.ajax({
+    url: 'http://localhost:3000/api/' + url,
+    method: method,
+    data: data,
+    beforeSend: setHeaders
+  }).done(function(res) {
+    return callback(res);
+  }).fail(function(err) {
+    console.error(err);
+  });
+}
