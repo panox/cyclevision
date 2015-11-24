86665bc5d17236da74deac028cd87843e869e3f0
panox <panospalos@gmail.com>
2015-11-24 10:51:32 +0000

    edit form appended to page

--- front-end/js/user-edit.js
+++ front-end/js/user-edit.js
@@ -1,5 +1,7 @@
 $(function(){
 
-  
+  var underscoreTemplate = _.template($('#user-edit-form').html());
+  var compiledTemplate = underscoreTemplate();
+  $('#user-edit').append(compiledTemplate);
 
 });
