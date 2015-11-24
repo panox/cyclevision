250e4286b7b69c908d374c2790bf90f860a2bb36
panox <panospalos@gmail.com>
2015-11-24 11:06:22 +0000

    get current user data

--- front-end/js/user-edit.js
+++ front-end/js/user-edit.js
@@ -1,7 +1,13 @@
 $(function(){
 
-  var underscoreTemplate = _.template($('#user-edit-form').html());
-  var compiledTemplate = underscoreTemplate();
-  $('#user-edit').append(compiledTemplate);
+  var userId = localStorage.getItem("userId");
+  console.log(userId);
+
+  ajaxRequest("users/" + userId, "get", null, function(res) {
+    console.log(res)
+    var underscoreTemplate = _.template($('#user-edit-form').html());
+    var compiledTemplate = underscoreTemplate();
+    $('#user-edit').append(compiledTemplate);
+  })
 
 });
