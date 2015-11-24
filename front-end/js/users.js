e5884386d5984d180e7d3a6c942abaf8e3c3c58a
panox <panospalos@gmail.com>
2015-11-24 09:59:43 +0000

    conditional statements for users attributes

--- front-end/js/users.js
+++ front-end/js/users.js
@@ -4,8 +4,11 @@ $.get("http://localhost:3000/api/users")
   .done(function(res){
   console.log(res.users)
   _(res.users).each(function(item) {
+    source = {first_name: null}
+    data = _.extend(source, item)
     var underscoreTemplate = _.template($('#all-users').html());
-    var compiledTemplate = underscoreTemplate(item);
+    console.log(item)
+    var compiledTemplate = underscoreTemplate(data);
     $('#users').append(compiledTemplate);
     })
   })
