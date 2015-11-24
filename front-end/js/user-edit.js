b110acf85864b71dbca12c985e342ee139b2bc00
panox <panospalos@gmail.com>
2015-11-24 11:36:19 +0000

    update form sends request to api path

--- front-end/js/user-edit.js
+++ front-end/js/user-edit.js
@@ -1,11 +1,14 @@
-$(function(){
+function updateUser(userId, formInfo) {
+  console.log(userId, formInfo);
+  ajaxRequest("users/" + userId, "put", formInfo, function(res){
+    console.log(res);
+  });
+}
 
-  var userId = localStorage.getItem("userId");
-  console.log(userId);
-
+function populateUserData(userId) {
   ajaxRequest("users/" + userId, "get", null, function(res) {
     var user = res.user;
-    console.log(user.local.email)
+
     var data = {
       email: user.local.email,
       first_name: user.first_name,
@@ -14,15 +17,30 @@ $(function(){
       type_of_cyclist: user.type_of_cyclist, 
       about_me:  user.about_me,
       city: user.city
+    };
+
+    /*
+    for(var name in data) {
+      $('[name=' + name + ']').val(data[name]);
     }
+    */
+
     var underscoreTemplate = _.template($('#user-edit-form').html());
     var compiledTemplate = underscoreTemplate(data);
     $('#user-edit').append(compiledTemplate);
-  })
+  });
+}
 
-  var formInfo = $('#user-edit-form').serialize();
-  ajaxRequest("users/" + userId, "put", formInfo, function(res){
-    console.log('form')
-  })
+$(function(){
+  console.log("USER EDIT LOADED");
+  var userId = localStorage.getItem("userId");
+
+  populateUserData(userId);
+
+  $('#user-edit').on('submit', 'form', function() {
+    event.preventDefault();
+    updateUser(userId, $(this).serialize());
+  });
+  
 
 });
