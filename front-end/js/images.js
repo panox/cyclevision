1ba84f53a7deb301232f81aacb08b804966608da
panox <panospalos@gmail.com>
2015-11-25 14:25:26 +0000

    images user id

--- front-end/js/images.js
+++ front-end/js/images.js
@@ -2,6 +2,7 @@ $(function(){
 
   ajaxRequest("images", "get", null, function(res) {
     _(res.images).each(function(item) {
+      console.log(item)
       imageUrl = configKeys.bucketUrl + item.image;
       item["image"] = imageUrl;
       var underscoreTemplate = _.template($('#all-images').html());
