bce7f77766e0af9f818ad12b31964ef39c959f37
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-25 18:19:24 +0000

    user page redirect

--- front-end/js/images.js
+++ front-end/js/images.js
@@ -2,7 +2,7 @@ $(function(){
 
   ajaxRequest("images", "get", null, function(res) {
     _(res.images).each(function(item) {
-      console.log(item)
+      console.log(item._id)
       imageUrl = configKeys.bucketUrl + item.image;
       item["image"] = imageUrl;
       var underscoreTemplate = _.template($('#all-images').html());
