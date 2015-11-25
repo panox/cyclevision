21af345242ca8d34b24209991318f4c1825ef607
panox <panospalos@gmail.com>
2015-11-25 15:04:26 +0000

    map created before

--- front-end/js/map.js
+++ front-end/js/map.js
@@ -2,21 +2,21 @@ $(function() {
 
   $.get("http://localhost:3000/api/images/5652162845ace0f652a568a3")
   .done(function(res){
-    var imageLocation = res.image.location
-    var secondHome = "https://maps.googleapis.com/maps/api/geocode/json?address=" + imageLocation
-    var picture = res.image.image
+    var itemLocation = item.location
+    var itemGoogle = "https://maps.googleapis.com/maps/api/geocode/json?address=" + itemLocation
+    var picture = item.image
 
-    $.get(secondHome)
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: myLatLng
+    });
+    
+    $.get(itemGoogle)
     .done(function(res){
       var location = res.results[0].geometry.location
 
       var myLatLng = {lat: location.lat, lng: location.lng};
 
-
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 18,
-        center: myLatLng
-      });
 
       var infowindow = new google.maps.InfoWindow({
           content: '<img src='+ picture +' height="80" width="80">'
