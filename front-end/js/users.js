de0f36b9f81ae8f74fdc543ebb7c0ca68a35b07c
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-25 20:14:58 +0000

    other user profile working

--- front-end/js/users.js
+++ front-end/js/users.js
@@ -1,22 +1,35 @@
 $(function(){
 
-  ajaxRequest("users", "GET", null, function(res){
-  console.log(res.users)
-  _(res.users).each(function(item) {
-    source = {
-      first_name: null,
-      last_name: null,
-      profile_pic: null, 
-      type_of_cyclist: null,
-      about_me: null, 
-      city: null
+  var otherId = localStorage.getItem("otherUser");
+  console.log(otherId)
+  
+  var url = "users/" + otherId;
+
+  ajaxRequest(url, "GET", null, function(res){
+
+    var user = res.user;
+    var data = {
+      email: user.local.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      profile_pic: user.profile_pic,
+      type_of_cyclist: user.type_of_cyclist,
+      about_me: user.about_me
     }
-    data = _.extend(source, item)
-    var underscoreTemplate = _.template($('#all-users').html());
-    console.log(item)
-    var compiledTemplate = underscoreTemplate(data);
-    $('#users').append(compiledTemplate);
+      source = {
+        first_name: null,
+        last_name: null,
+        profile_pic: null, 
+        type_of_cyclist: null,
+        about_me: null, 
+        city: null
+      }
+      data = _.extend(source, user)
+      var underscoreTemplate = _.template($('#all-users').html());
+      console.log(user)
+      var compiledTemplate = underscoreTemplate(data);
+      $('#users').append(compiledTemplate);
+      localStorage.removeItem("otherUser");
     })
-  })
 
 });
