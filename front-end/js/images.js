577d79288b4d8f052dcc7591de0914a574449e43
panox <panospalos@gmail.com>
2015-11-25 09:56:12 +0000

    ajax request from token.js

--- front-end/js/images.js
+++ front-end/js/images.js
@@ -1,7 +1,6 @@
 $(function(){
 
-  $.get("http://localhost:3000/api/images")
-  .done(function(res){
+  ajaxRequest("images", "get", null, function(res) {
     _(res.images).each(function(item) {
       var underscoreTemplate = _.template($('#all-images').html());
       var compiledTemplate = underscoreTemplate(item);
@@ -9,10 +8,6 @@ $(function(){
     })
 
     $('.materialboxed').materialbox();
-
-  })
-  .fail(function(res){
-    console.log("Error getting values")
   })
   
 
