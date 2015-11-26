bdf747f881714fc64566cde9e272752cb23548f6
panox <panospalos@gmail.com>
2015-11-26 14:36:53 +0000

    heroku api link in config

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ if(document.domain == "localhost"){ // development
   }
 } else { //production
   window.configKeys = {
-    url: 'http://herokusomething/api/',
+    url: 'https://stark-stream-4100.herokuapp.com/api/',
     bucketUrl: 'https://s3-eu-west-1.amazonaws.com/cyclevision/'
   }
 }
