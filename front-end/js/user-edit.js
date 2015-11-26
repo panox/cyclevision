a724c1b9d7c354f61c7d6b8069363780a2084a6d
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-11-26 11:32:33 +0000

    edit user workinf

--- front-end/js/user-edit.js
+++ front-end/js/user-edit.js
@@ -4,16 +4,8 @@ function deleteUser(userId) {
   });
 }
 
-function updateUser(userId, formInfo) {
-  ajaxRequest("users/" + userId, "put", formInfo, function(res){
-    populateUserData(userId);
-    console.log('update success');
-  });
-}
-
 function populateUserData(userId) {
   ajaxRequest("users/" + userId, "get", null, function(res) {
-    console.log("res", res)
     var user = res.user;
 
     var data = {
@@ -37,25 +29,26 @@ function populateUserData(userId) {
     var compiledTemplate = underscoreTemplate(data);
     $('#user-edit-form').prepend(compiledTemplate);
 
+
+    //update user
     $('.modal-content').find('input#submit').on('click', function() {
       event.preventDefault();
-      console.log(userId)
       var $cardTitle = ($('#current-user-card'))
-      console.log($cardTitle)
       var data = {
         email: $('#'+'local.email').val(),
         first_name: $('#'+'first_name').val(),
         last_name: $('#'+'last_name').val(),
         profile_pic: $('#'+'profile_pic').val(),
         type_of_cyclist: $('#'+'type_of_cyclist').val(),
+        about_me: $('#'+'about_me').val(),
         city: $('#'+'city').val()
       }
-      $cardTitle.find('#local.email').text(data.email);
-      $cardTitle.find('#first_name').text(data.first_name);
-      $cardTitle.find('#last_name').text(data.last_name);
-      $cardTitle.find('#profile_pic').text(data.profile_pic);
-      $cardTitle.find('#type_of_cyclist').text(data.type_of_cyclist);
-      $cardTitle.find('#city').text(data.city);
+      $cardTitle.find('#-email').text(data.email);
+      $cardTitle.find('#-cyclist-name').text(data.first_name + " " + data.last_name);
+      $cardTitle.find('#-profile_pic').text(data.profile_pic);
+      $cardTitle.find('#-about_me').text(data.about_me);
+      $cardTitle.find('#-type_of_cyclist').text(data.type_of_cyclist);
+      $cardTitle.find('#-city').text(data.city);
 
       ajaxRequest("users/" + userId, "PUT", data, function(){} )
       $('.lean-overlay').remove()
